012f76242909eee7aef95b7eefc179da264a6902
fabroche <kraxusmmo@gmail.com>
2025-03-02 16:46:29 +0000

    refactor: e-commerce now is just for contain models proposals

--- src/api/e-commerce.js
+++ src/api/e-commerce.js
@@ -1,32 +1,3 @@
-const {ENV} = require('../../config');
-const {faker} = require('@faker-js/faker');
-const {createArrayFromObject} = require('../utils/utils.js')
-const {ProductModel} = require("../models/product.models");
-const {CategoryModel} = require("../models/category.models");
-
-function getCategoryNames(categoriesIdList) {
-  return categories.filter(category => categoriesIdList.includes(category.id));
-}
-
-const categories = createArrayFromObject(5, () => (new CategoryModel({
-  id: faker.database.mongodbObjectId(),
-  name: faker.commerce.productAdjective(),
-})))
-
-const products = createArrayFromObject(20, () => (new ProductModel({
-  id: faker.database.mongodbObjectId(),
-  name: faker.commerce.productName(),
-  price: faker.commerce.price(),
-  currency: ENV.CURRENCY,
-  categories: createArrayFromObject(
-    Math.floor(Math.random() * categories.length) + 1
-    , () => (
-      categories[Math.floor(Math.random() * categories.length)].id
-    )),
-  description: faker.commerce.productDescription(),
-  image: faker.image.url({width: ENV.PRODUCT_IMAGE_W, height: ENV.PRODUCT_IMAGE_H}),
-})))
-
 const users = [
   {
     id: '1',
@@ -87,10 +58,7 @@ const permissions = [
 ]
 
 module.exports = {
-  products,
-  categories,
   users,
   roles,
   permissions,
-  getCategoryNames,
 }
