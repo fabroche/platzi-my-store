0be2585cdd289b1926eaaefc32e7654f3e6bcca5
fabroche <kraxusmmo@gmail.com>
2025-03-04 22:12:27 +0000

    refactor: now e-commerce.js handle all the data getting logic.

--- src/api/e-commerce.js
+++ src/api/e-commerce.js
@@ -1,3 +1,38 @@
+const {createArrayFromObject} = require("../utils/utils");
+const {ProductModel} = require("../models/product.models.js");
+const {faker} = require("@faker-js/faker");
+const {ENV} = require("../../config");
+const {CategoryModel} = require("../models/category.models");
+
+function generateProducts({limit = 5, categories = []}) {
+
+  return createArrayFromObject(limit, () => (new ProductModel({
+    id: faker.database.mongodbObjectId(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    currency: ENV.CURRENCY,
+    categories: createArrayFromObject(
+      Math.floor(Math.random() * categories.length) + 1
+      , () => (
+        categories[Math.floor(Math.random() * categories.length)].id
+      )),
+    description: faker.commerce.productDescription(),
+    image: faker.image.url({width: ENV.PRODUCT_IMAGE_W, height: ENV.PRODUCT_IMAGE_H}),
+  })))
+}
+
+function generateCategories({limit = 5}) {
+  return createArrayFromObject(limit, () => (new CategoryModel({
+    id: faker.database.mongodbObjectId(),
+    name: faker.commerce.productAdjective(),
+  })))
+}
+
+const categories = generateCategories({limit: 5});
+
+const products = generateProducts({limit: 20, categories: categories});
+
+
 const users = [
   {
     id: '1',
@@ -61,4 +96,6 @@ module.exports = {
   users,
   roles,
   permissions,
+  categories,
+  products,
 }
