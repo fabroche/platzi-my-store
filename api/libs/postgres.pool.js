9f7ed00024f10c18b0ecf3c23771ac24da6c394b
fabroche <kraxusmmo@gmail.com>
2025-03-14 16:30:57 +0000

    refactor: removed unused constants

--- api/libs/postgres.pool.js
+++ api/libs/postgres.pool.js
@@ -1,23 +1,8 @@
 const {Pool} = require('pg');
 const {ENV} = require('../config.js');
 
-  const DB_USER = encodeURIComponent(ENV.POSTGRES_USER)
-  const DB_PASSWORD = encodeURIComponent(ENV.POSTGRES_PASSWORD)
-
-  const URI = `postgres://${DB_USER}:${DB_PASSWORD}@${ENV.POSTGRES_HOST}:${ENV.POSTGRES_PORT}/${ENV.POSTGRES_DB}`;
-  const SUPABASE_URI = ENV.SUPABASE_DB;
-
-  // let ssl = null;
-  //
-  // if (process.env.CA_CERT) {
-  //   ssl = {
-  //     rejectUnauthorized: true,
-  //     ca: process.env.CA_CERT,
-  //   }
-  // }
-
   const pool = new Pool({
-    connectionString: process.env.SUPABASE_VERCEL_URI || ENV.SUPABASE_DB,
+    connectionString: process.env.SUPABASE_VERCEL_URI || ENV.SUPABASE_VERCEL_URI,
     ssl: {
       rejectUnauthorized: false,
     },
