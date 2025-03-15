c75bc5d8a82b90d9bbe210cb429e37f5b2c0b56d
fabroche <kraxusmmo@gmail.com>
2025-03-15 17:27:19 +0000

    fix: bug on incorrect exportation.

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -3,7 +3,6 @@ const {config} = require('../config');
 
 const sequelize = new Sequelize(config.db.connectionString, {
   dialect: 'postgres',
-  logging: true
 });
 
-module.exports.sequelize = {sequelize};
+module.exports = {sequelize};
