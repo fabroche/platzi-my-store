748f9bda4d304638d3fa476b49fe050fb277ebdb
fabroche <kraxusmmo@gmail.com>
2025-03-15 18:58:10 +0000

    feat: integrating sequelize on user.service

--- api/services/user.service.js
+++ api/services/user.service.js
@@ -1,20 +1,16 @@
 const boom = require('@hapi/boom');
 
-const {pool} = require('../libs/postgres.pool.js');
+const {sequelize: {models}} = require('../libs/sequelize');
 
 class UserService {
-  constructor() {
-    this.pool = pool;
-    this.pool.on('error', (err) => console.log(err));
-  }
 
   async create(data) {
     return data;
   }
 
   async find() {
-    const rta = await pool.query('SELECT * FROM task');
-    return rta.rows;
+    const rta = await models.User.findAll();
+    return rta;
   }
 
   async findOne(id) {
