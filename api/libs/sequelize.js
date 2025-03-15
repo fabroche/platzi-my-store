f7fcf58a247209f90d8983c9d1545b8f7a50362c
fabroche <kraxusmmo@gmail.com>
2025-03-15 18:57:30 +0000

    feat: syncing the models

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -1,8 +1,23 @@
 const {Sequelize} = require('sequelize');
 const {config} = require('../config');
+const {setUpModels} = require('../../db/models');
 
 const sequelize = new Sequelize(config.db.connectionString, {
   dialect: 'postgres',
 });
+
+setUpModels(sequelize);
+
+sequelize.sync()
+  .then(() => {
+    console.group('Sequelize Sync')
+    console.log('Successfully Sync Models')
+    console.groupEnd()
+  })
+  .catch(err => {
+    console.group('Sequelize Sync')
+    console.error(err)
+    console.groupEnd()
+  });
 
 module.exports = {sequelize};
