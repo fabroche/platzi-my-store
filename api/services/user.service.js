f47f6ce98996c3ea88b01677b3463446a1a9d877
fabroche <kraxusmmo@gmail.com>
2025-03-19 14:31:52 +0000

    feat: User CRUD implemented

--- api/services/user.service.js
+++ api/services/user.service.js
@@ -1,31 +1,76 @@
 const boom = require('@hapi/boom');
 
 const {sequelize: {models}} = require('../libs/sequelize');
+const {UserModel} = require('../src/models/user.model');
+const {values} = require("pg/lib/native/query");
+
 
 class UserService {
 
   async create(data) {
-    return data;
+
+    const newUser = new UserModel(data)
+
+    if (!newUser.isValid()) {
+      throw boom.badData('Incorrect user data')
+    }
+
+    const response = await models.User.create(newUser);
+
+    return new UserModel(response.dataValues);
   }
 
   async find() {
     const rta = await models.User.findAll();
+
+    if (!rta?.length) {
+      throw boom.notFound("No se encontraron usuarios")
+    }
+
     return rta;
   }
 
   async findOne(id) {
-    return { id };
+    const findedUser = await models.User.findByPk(id);
+
+    if (!findedUser?.dataValues) {
+      throw boom.notFound(`There is no user with id=${id}`)
+    }
+
+    return new UserModel(findedUser.dataValues);
   }
 
   async update(id, changes) {
+
+    const updatedUser = await this.findOne(id);
+
+    await models.User.update(changes, {
+      where: {
+        id: id
+      }
+    })
+
     return {
-      id,
-      changes,
+      ...updatedUser,
+      changes: changes
     };
   }
 
   async delete(id) {
-    return { id };
+
+    const deletedUser = await this.findOne(id);
+
+    const response = await models.User.destroy({
+      where: {
+        id: id
+      }
+    })
+
+    if (!response > 0) {
+      throw boom.notFound("No se encontraron usuarios")
+    }
+
+    return deletedUser;
   }
 }
 
