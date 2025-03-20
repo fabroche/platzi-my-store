1dd09c4338eee52b20c9fe03ae25614de51b451d
fabroche <kraxusmmo@gmail.com>
2025-03-20 15:55:04 +0000

    refactor: extracted cors settings logic and custom middlewares logic into their owns files for better readability.

--- index.js
+++ index.js
@@ -2,40 +2,28 @@ const {ENV,config} = require('./api/config.js');
 
 const express = require('express');
 const {routerApi} = require('./api/routes');
-const {
-  logErrorsMiddleware,
-  errorHandlerMiddleware,
-  boomErrorHandlerMiddleware,
-  ormErrorHandlerMiddleware
-} = require('./api/middlewares/error.handler.js');
 const cors = require("cors");
+const {corsOptions} = require("./api/middlewares/cors.handler");
+const {setCustomMiddlewares} = require("./api/middlewares");
 
 const app = express();
 const port = ENV.PORT || 3000;
+
+// parsing json middleware
 app.use(express.json());
 
-const whitelist = ENV.CORS_WHITE_LIST.split(',');
-
-const options = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error(`Unknown origin: ${origin}`), false);
-    }
-  }
-}
-app.use(cors(options));
+// defining cors
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send("Genzai with Express on Vercel")
 })
 
+// defining routes
 routerApi(app);
-app.use(logErrorsMiddleware);
-app.use(ormErrorHandlerMiddleware);
-app.use(boomErrorHandlerMiddleware);
-app.use(errorHandlerMiddleware);
+
+// defining custom middlewares
+setCustomMiddlewares(app);
 
 app.listen(port, () => {
   console.log(`Listening on port :${port}`);
