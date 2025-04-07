d2da7c76ed63c0f76d48ca7fde3c120cd51bf8af
fabroche <kraxusmmo@gmail.com>
2025-04-07 19:05:01 +0100

    feat: created customer model.

---
+++ api/src/models/customer.model.js
@@ -0,0 +1,44 @@
+const {Model, DataTypes, Sequelize} = require('sequelize');
+
+const CUSTOMER_TABLE = 'customer';
+
+
+const CustomerSchema = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'last_name',
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: 'created_at',
+    defaultValue: DataTypes.NOW,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'user_id',
+    unique: true,
+    references: {
+      model: CUSTOMER_TABLE,
+      key: 'id',
+    },
+  },
+  onUpdate: 'CASCADA',
+  onDelete: 'SET NULL',
+}
