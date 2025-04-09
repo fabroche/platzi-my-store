b6908d12be5a31b4b91e0532d3a9551446516971
fabroche <kraxusmmo@gmail.com>
2025-04-09 21:03:56 +0100

    fix: typo with onUpdate attr CASCADA => CASCADE. feat: implemented the one to one relation with User.

--- db/models/customer.model.js
+++ db/models/customer.model.js
@@ -1,5 +1,6 @@
 const {Model, DataTypes, Sequelize} = require('sequelize');
 
+const {USER_TABLE} = require('./user.model');
 const CUSTOMER_TABLE = 'customer';
 
 
@@ -35,17 +36,17 @@ const CustomerSchema = {
     field: 'user_id',
     unique: true,
     references: {
-      model: CUSTOMER_TABLE,
+      model: USER_TABLE,
       key: 'id',
     },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
   },
-  onUpdate: 'CASCADA',
-  onDelete: 'SET NULL',
 }
 
 class Customer extends Model {
-  static associate() {
-
+  static associate(models) {
+    this.belongsTo(models.User, {as: 'user'})
   }
 
   static config(sequelize) {
