ba008eda70665362cf27e4684d68fa1e8a0e1d82
fabroche <kraxusmmo@gmail.com>
2025-04-09 20:58:59 +0100

    feat: validation implemented for not throw an error when the role column already exist

--- db/migrations/20250320160120-user-add-role.js
+++ db/migrations/20250320160120-user-add-role.js
@@ -5,13 +5,21 @@ const {UserSchema, USER_TABLE} = require('../models/user.model')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-
-    await queryInterface.addColumn(USER_TABLE, 'role', UserSchema.role);
+    try {
+      await queryInterface.addColumn(USER_TABLE, 'role', UserSchema.role);
+    } catch (error) {
+      if (error.message.includes(`column "role" of relation "${USER_TABLE}" already exists`)) {
+        console.info(`La columna role ya existe en la tabla ${USER_TABLE}, continuando...`)
+      } else {
+        throw error;
+      }
+    }
 
   },
 
   async down(queryInterface, Sequelize) {
 
     await queryInterface.removeColumn(USER_TABLE, 'role');
+
   }
 };
