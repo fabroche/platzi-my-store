3f51513f1aa12aff2e96528137f0edcc042738e5
fabroche <kraxusmmo@gmail.com>
2025-04-09 19:38:03 +0100

    feat: customer router implemented

---
+++ api/routes/customers.router.js
@@ -0,0 +1,60 @@
+const express = require('express');
+
+const {CustomersService} = require('../services/customers.service');
+const {validatorHandler} = require('../middlewares/validator.handler');
+
+const {
+  createCustomerSchema,
+  updateCustomerSchema,
+  getCustomerSchema,
+} = require('../schemas/customer.schema')
+
+const customersRouter = express.Router();
+const customerService = new CustomersService();
+
+customersRouter.get('/', async (req, res, next) => {
+  try {
+    res.json(await customerService.find());
+  } catch (error) {
+    next(error);
+  }
+})
+
+customersRouter.post('/',
+  validatorHandler(createCustomerSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      res.status(201).json(await customerService.create(body));
+    } catch (error) {
+      next(error);
+    }
+  });
+
+customersRouter.patch('/:id',
+  validatorHandler(getCustomerSchema, 'params'),
+  validatorHandler(updateCustomerSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const {id} = req.params;
+      const body = req.body;
+      res.status(201).json(await customerService.update(id, body));
+    } catch (error) {
+      next(error);
+    }
+  });
+
+customersRouter.delete('/:id',
+  validatorHandler(getCustomerSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const {id} = req.params;
+      res.status(200).json(await customerService.delete(id));
+    } catch (error) {
+      next(error);
+    }
+  });
+
+module.exports = {
+  customersRouter
+};
