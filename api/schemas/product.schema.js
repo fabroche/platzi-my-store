e0c3a9d02a8812ad6be07859512bfc14d9dc4590
fabroche <kraxusmmo@gmail.com>
2025-04-16 20:41:43 +0100

    feat: new attrs for product.schema, and new validationSchema Object

--- api/schemas/product.schema.js
+++ api/schemas/product.schema.js
@@ -1,21 +1,39 @@
 const Joi = require('joi');
+const {generateKeyMap} = require("../utils/utils");
 
-const id = Joi.string().uuid();
-const name = Joi.string().alphanum().min(3).max(15);
-const price = Joi.number().integer().min(10);
+
+const productValidationSchema = Joi.object({
+  id: Joi.string().uuid(),
+  name: Joi.string().alphanum().min(3).max(15),
+  price: Joi.number().integer().min(10),
+  description: Joi.string().min(10),
+  image: Joi.string().uri(),
+})
+
+const productsAttrTypes = generateKeyMap(productValidationSchema.describe().keys);
 
 const createProductSchema = Joi.object({
-  name: name.required(),
-  price: price.required(),
+  name: productValidationSchema.extract(productsAttrTypes.name).required(),
+  price: productValidationSchema.extract(productsAttrTypes.price).required(),
+  description: productValidationSchema.extract(productsAttrTypes.description).required(),
+  image: productValidationSchema.extract(productsAttrTypes.image).required(),
 });
 
 const updateProductSchema = Joi.object({
-  name: name,
-  price: price,
+  name: productValidationSchema.extract(productsAttrTypes.name),
+  price: productValidationSchema.extract(productsAttrTypes.price),
+  description: productValidationSchema.extract(productsAttrTypes.description),
+  image: productValidationSchema.extract(productsAttrTypes.image)
 });
 
 const getProductSchema = Joi.object({
-  id: id.required(),
+  id: productValidationSchema.extract(productsAttrTypes.id).required(),
 });
 
-module.exports = { createProductSchema, updateProductSchema, getProductSchema }
+module.exports = {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+  productValidationSchema,
+  productsAttrTypes,
+}
