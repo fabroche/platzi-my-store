d8da9cb146e8e8c6376dd1c382fec73be575fdb2
fabroche <kraxusmmo@gmail.com>
2025-04-17 14:12:30 +0100

    feat: implemented category service

---
+++ api/services/categories.service.js
@@ -0,0 +1,68 @@
+const {CategoryModel} = require("../src/models/category.models");
+const {generateCategories, saveItemsIntoDB} = require("../src/api/e-commerce");
+const boom = require("@hapi/boom");
+const {sequelize: {models}} = require("../libs/sequelize");
+
+class CategoryService {
+  constructor() {
+  }
+
+
+  async generate() {
+    try {
+
+      const generatedCategories = generateCategories({limit: 5});
+
+      await saveItemsIntoDB({
+        data: generatedCategories,
+        tableName: this.modelName,
+      });
+
+      return await this.getCategories()
+
+    } catch (error) {
+      throw boom.badData(error.message);
+    }
+
+  }
+
+  async find() {
+    const categories = await models.Category.findAll();
+
+    if (!categories?.length) {
+      throw boom.notFound('There is not any Category');
+    }
+    return categories;
+  }
+
+  async findOne(id) {
+    const category = await models.Category.findByPk(id)
+
+    if (!category) {
+      throw boom.notFound('Category not found');
+    }
+
+    return category;
+  }
+
+  async update(id, changes) {
+    const category = await this.findOne(id);
+    const rta = await category.update(changes);
+    return rta;
+  }
+
+  async create(data) {
+    const newCategory = await models.Category.create(data);
+    return newCategory;
+  }
+
+  async delete(id) {
+    const category = await this.findOne(id);
+    await category.destroy();
+    return {
+      rta: true
+    }
+  }
+}
+
+module.exports = {CategoryService};
