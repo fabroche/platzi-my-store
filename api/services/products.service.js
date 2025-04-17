86acc1002bb53ce515b6361871d53f79b3febc06
fabroche <kraxusmmo@gmail.com>
2025-04-17 13:51:29 +0100

    feat: re implemented Product Service now using sequelize orm

---
+++ api/services/products.service.js
@@ -0,0 +1,84 @@
+const {sequelize: {models}} = require('../libs/sequelize');
+const {generateProducts, saveItemsIntoDB} = require("../src/api/e-commerce");
+const boom = require("@hapi/boom");
+
+
+class ProductsService {
+
+  async generate({categories}) {
+    const generatedProducts = generateProducts({
+      limit: 100,
+      categories: categories
+    });
+
+    generatedProducts.map(async (product) => {
+      const productCategories = [...product.categories];
+
+      delete product.categories;
+
+      await saveItemsIntoDB({
+        tableName: this.modelName,
+        data: [product],
+        keepOldData: true
+      })
+
+      await Promise.all(productCategories.map(async (categoryId) => {
+        await saveItemsIntoDB({
+          tableName: "product_categories",
+          data: [{product_id: product.id, category_id: categoryId}],
+          keepOldData: true
+        })
+      }))
+    })
+
+    return await this.getProducts();
+  }
+
+  async find() {
+    const products = await models.Product.findAll({
+      include: ['category']
+    });
+
+    if (!products?.length) {
+      throw boom.notFound("No se encontraron productos")
+    }
+
+    return products;
+  }
+
+  async findOne(id) {
+    const product = await models.Product.findByPk(id, {
+      include: ['category']
+    });
+
+    if (!product) {
+      throw boom.notFound(`No se existe ningun producto con id = ${id}`)
+    }
+
+    return product;
+  }
+
+  async update(id, changes) {
+    const product = await this.findOne(id);
+    const rta = await product.update(changes);
+    return rta;
+  }
+
+  async create(data) {
+    const newProduct = await models.Product.create(data, {
+      include: ['category']
+    });
+    return newProduct;
+  }
+
+  async delete(id) {
+    const product = await this.findOne(id);
+    await product.destroy();
+    return {
+      rta: true
+    }
+  }
+
+}
+
+module.exports = {ProductsService};
