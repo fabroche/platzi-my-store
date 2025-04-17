51e7ff5936c7d35a2f1db1901f83a2333f03c73d
fabroche <kraxusmmo@gmail.com>
2025-04-17 14:24:42 +0100

    feat: re-implementing category service

--- api/routes/categories.router.js
+++ api/routes/categories.router.js
@@ -1,16 +1,16 @@
 const {handlePagination} = require("../src/utils/utils");
+const {validatorHandler} = require("../middlewares/validator.handler");
 const {Router} = require("express");
-const {CategoriesService} = require("../services/categories.services");
-const {ProductsService} = require("../services/products.services");
+const {CategoryService} = require("../services/categories.service");
+const {createCategorySchema} = require("../schemas/category.schema");
 
 const categoriesRouter = Router();
 
-const categoriesService = new CategoriesService();
-const productsService = new ProductsService();
+const categoriesService = new CategoryService();
 
 categoriesRouter.get('/', async (req, res, next) => {
   try {
-    const categories = await categoriesService.getCategories();
+    const categories = await categoriesService.find();
 
     const {limit = categories.length, offset = 0} = req.query;
 
@@ -33,125 +33,125 @@ categoriesRouter.get('/', async (req, res, next) => {
   }
 })
 
-categoriesRouter.get('/generate', async (req, res, next) => {
-  try {
+// categoriesRouter.get('/generate', async (req, res, next) => {
+//   try {
+//
+//     const result = await categoriesService.generate();
+//
+//     res.status(200).json(result)
+//
+//   } catch (err) {
+//     next(err);
+//   }
+// })
+//
+// categoriesRouter.get('/:id', async (req, res, next) => {
+//   try {
+//     const {id} = req.params;
+//     const result = await categoriesService.findById({id})
+//
+//     res.json({
+//       ...result
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// })
+//
+// categoriesRouter.get('/:categoryId/products/:productId', async (req, res, next) => {
+//   try {
+//     const {
+//       categoryId,
+//       productId
+//     } = req.params;
+//
+//     const products = await productsService.getProducts();
+//
+//     const {
+//       limit = products.length,
+//       offset = 0
+//     } = req.query;
+//
+//     if (limit <= 0 || offset < 0) {
+//       res.status(400).send('Bad Request: Limit tiene que ser mayor que 0 y Offset no puede se menor que 0');
+//     }
+//
+//     let result = await categoriesService.getCategoryInProduct({
+//       categoryId,
+//       productId,
+//       products
+//     });
+//
+//     result = handlePagination({
+//       limit,
+//       offset,
+//       itemList: result
+//     })
+//
+//     res.json({
+//       ...result,
+//       categories: await categoriesService.getCategoryNamesByIdList({categoriesIdList: [categoryId]})
+//     })
+//   } catch (error) {
+//     next(error);
+//   }
+//
+// })
+//
+// categoriesRouter.get('/:categoryId/products', async (req, res, next) => {
+//   try {
+//     const {
+//       categoryId,
+//     } = req.params;
+//
+//     const products = await productsService.getProducts();
+//
+//     const {
+//       limit = products.length,
+//       offset = 0
+//     } = req.query;
+//
+//     if (limit <= 0 || offset < 0) {
+//       res.status(400).send('Bad Request: Limit tiene que ser mayor que 0 y Offset no puede se menor que 0');
+//     }
+//
+//     let result = await categoriesService.getCategoryInProductsById({
+//       categoryId,
+//       products
+//     })
+//
+//     if (!result?.length) {
+//       res.status(404).send(`No existe ningun producto con la categoria = ${categoryId}`);
+//     }
+//
+//     result = handlePagination({
+//       limit,
+//       offset,
+//       itemList: result
+//     })
+//
+//     res.json([
+//       ...result
+//     ])
+//   } catch (error) {
+//     next(error);
+//   }
+//
+// })
 
-    const result = await categoriesService.generate();
+categoriesRouter.post('/',
+  validatorHandler(createCategorySchema, 'body'),
+  async (req, res, next) => {
 
-    res.status(200).json(result)
-
-  } catch (err) {
-    next(err);
-  }
-})
-
-categoriesRouter.get('/:id', async (req, res, next) => {
-  try {
-    const {id} = req.params;
-    const result = await categoriesService.findById({id})
-
-    res.json({
-      ...result
-    });
-  } catch (error) {
-    next(error);
-  }
-})
-
-categoriesRouter.get('/:categoryId/products/:productId', async (req, res, next) => {
-  try {
-    const {
-      categoryId,
-      productId
-    } = req.params;
-
-    const products = await productsService.getProducts();
-
-    const {
-      limit = products.length,
-      offset = 0
-    } = req.query;
-
-    if (limit <= 0 || offset < 0) {
-      res.status(400).send('Bad Request: Limit tiene que ser mayor que 0 y Offset no puede se menor que 0');
-    }
-
-    let result = await categoriesService.getCategoryInProduct({
-      categoryId,
-      productId,
-      products
-    });
-
-    result = handlePagination({
-      limit,
-      offset,
-      itemList: result
-    })
-
-    res.json({
-      ...result,
-      categories: await categoriesService.getCategoryNamesByIdList({categoriesIdList: [categoryId]})
-    })
-  } catch (error) {
-    next(error);
-  }
-
-})
-
-categoriesRouter.get('/:categoryId/products', async (req, res, next) => {
-  try {
-    const {
-      categoryId,
-    } = req.params;
-
-    const products = await productsService.getProducts();
-
-    const {
-      limit = products.length,
-      offset = 0
-    } = req.query;
-
-    if (limit <= 0 || offset < 0) {
-      res.status(400).send('Bad Request: Limit tiene que ser mayor que 0 y Offset no puede se menor que 0');
-    }
-
-    let result = await categoriesService.getCategoryInProductsById({
-      categoryId,
-      products
-    })
-
-    if (!result?.length) {
-      res.status(404).send(`No existe ningun producto con la categoria = ${categoryId}`);
-    }
-
-    result = handlePagination({
-      limit,
-      offset,
-      itemList: result
-    })
-
-    res.json([
-      ...result
-    ])
-  } catch (error) {
-    next(error);
-  }
-
-})
-
-categoriesRouter.post('/', async (req, res, next) => {
   try {
     const body = req.body;
 
-    const newCategory = await categoriesService.createCategory({
-      category: body
-    })
+    const newCategory = await categoriesService.create(body)
 
     if (newCategory) {
       res.status(201).json({
         message: `Category ${newCategory.name} created`,
-        data: newCategory,
-        id: newCategory.id
+        data: newCategory
       });
     }
   } catch (error) {
