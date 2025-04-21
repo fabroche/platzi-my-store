aa2a6ec690b77aafdf9af00f40d48839e663e10f
fabroche <kraxusmmo@gmail.com>
2025-04-21 20:07:04 +0100

    feat: implemented orders services.

---
+++ api/services/orders.service.js
@@ -0,0 +1,68 @@
+const {generateCategories, saveItemsIntoDB} = require("../src/api/e-commerce");
+const boom = require("@hapi/boom");
+const {sequelize: {models}} = require("../libs/sequelize");
+
+class OrderService {
+  constructor() {
+  }
+
+
+  async generate() {
+    try {
+
+      const generatedCategories = generateCategories({limit: 5});
+
+      await saveItemsIntoDB({
+        data: generatedCategories,
+        tableName: this.modelName,
+      });
+
+      return await this.getCategories()
+
+    } catch (error) {
+      throw boom.badData(error.message);
+    }
+
+  }
+
+  async find() {
+    const orders = await models.Order.findAll();
+
+    if (!orders?.length) {
+      throw boom.notFound('There is not any Category');
+    }
+    return orders;
+  }
+
+  async findOne(id) {
+    const order = await models.Order.findByPk(id)
+
+    if (!order) {
+      throw boom.notFound('Category not found');
+    }
+
+    return order;
+  }
+
+  async update(id, changes) {
+    const order = await this.findOne(id);
+    const rta = await order.update(changes);
+    return rta;
+  }
+
+  async create(data) {
+    const newOrder = await models.Order.create(data);
+    return newOrder;
+  }
+
+  async delete(id) {
+    const order = await this.findOne(id);
+    await order.destroy();
+    return {
+      rta: true
+    }
+  }
+
+}
+
+module.exports = {OrderService};
