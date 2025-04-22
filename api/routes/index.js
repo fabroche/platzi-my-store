072110027baa1af2f3744319ea9b4d2bd073e34d
fabroche <kraxusmmo@gmail.com>
2025-04-22 19:57:42 +0100

    feat: enabled orders endpoints

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,7 @@ const {productsRouter} = require("./products.router.js");
 const {usersRouter} = require("./users.router.js");
 const {categoriesRouter} = require("./categories.router.js");
 const {customersRouter} = require("./customers.router.js");
+const {ordersRouter} = require("./orders.router");
 const {Router} = require('express');
 
 function routerApi(app) {
@@ -11,6 +12,7 @@ function routerApi(app) {
   routerV1.use("/users", usersRouter);
   routerV1.use("/categories", categoriesRouter);
   routerV1.use("/customers", customersRouter);
+  routerV1.use("/orders", ordersRouter);
 }
 
 module.exports = {routerApi};
