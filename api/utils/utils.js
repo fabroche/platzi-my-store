d391114dc8dd60ba50a88b23c6893a124b376faf
fabroche <kraxusmmo@gmail.com>
2025-05-03 13:58:03 +0100

    feat: added docs strings, feat: nuevo metodo que Setea los filtros de una consulta segun los atributos de un schema y los valores de una query.

--- api/utils/utils.js
+++ api/utils/utils.js
@@ -1,6 +1,14 @@
-//genera un map de las key de un objeto
-// ejemplodado un objeto {id: 1, name: test} devolvera {id: 'id', name: 'name'}
-
+/**
+ * Generates a key map from an object.
+ *
+ * @param {Object} obj - The object to map.
+ * @return {Object} A key map of the object.
+ *
+ * @example
+ * const obj = {id: 1, name: 'test'};
+ * const keyMap = generateKeyMap(obj);
+ * // keyMap is {id: 'id', name: 'name'}
+ */
 function generateKeyMap(obj) {
   return Object.keys(obj).reduce((map, key) => {
     map[key] = key;
@@ -8,6 +16,14 @@ function generateKeyMap(obj) {
   }, {});
 }
 
+/**
+ * Sets pagination options based on the query parameters.
+ *
+ * @param {Object} query - The query object containing pagination parameters.
+ * @param {number} query.limit - The maximum number of items to return.
+ * @param {number} query.offset - The number of items to skip before starting to collect the result set.
+ * @param {Object} options - The options object to modify with pagination settings.
+ */
 function setPagination(query, options) {
   const {limit, offset} = query;
 
@@ -17,7 +33,29 @@ function setPagination(query, options) {
   }
 }
 
+/**
+ * Setea los filtros de una consulta segun los atributos de un schema
+ * y los valores de una query.
+ *
+ * @param {Object} attrSchema - Schema de los atributos a filtrar.
+ * @param {Object} query - Valores de la query.
+ * @param {Object} options - Opciones de la consulta.
+ */
+function setFilters(attrSchema, query, options) {
+
+  Object.values(attrSchema).map((filter) => {
+    if (filter === attrSchema.limit || filter === attrSchema.offset) {
+      return;
+    }
+
+    if (query[filter]) {
+      options.where[filter] = query[filter];
+    }
+  })
+}
+
 module.exports = {
   generateKeyMap,
-  setPagination
+  setPagination,
+  setFilters
 }
